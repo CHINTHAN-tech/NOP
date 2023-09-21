0e4a36bdf650f2b1fffb1c2f443aaa61a6411bd6
chinthan <chinthan.v1997@gmail.com>
2023-09-21 19:35:38 +0530

    first commit

---
+++ products.js
@@ -0,0 +1,80 @@
+import product1 from './assset/2j.png'
+import product2 from './assset/3.png'
+import product3 from './assset/4.png'
+import product4 from './assset/5.png'
+import product5 from './assset/6y.png'
+import product6 from './assset/7.png'
+import product7 from './assset/8.png'
+import product8 from './assset/9.png'
+import product9 from './assset/10.png'
+import product10 from './assset/11.png'
+import product11 from './assset/tvs.png'
+
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "bike",
+    price: 10000,
+    productImage: product1,
+  },{
+    id: 2 ,
+    productName:"bike",
+    price:10000 ,
+    productImage:product2,
+  },
+  {
+    id:3,
+    productName:"bike" ,
+    price:10000 ,
+    productImage: product3,
+  },
+  {
+    id:4 ,
+    productName: "bike",
+    price: 10000,
+    productImage:product4 ,
+  },
+  {
+    id:5 ,
+    productName: "bike",
+    price: 10000,
+    productImage:product5 ,
+  },
+  {
+    id:6 ,
+    productName: "bike",
+    price:10000 ,
+    productImage: product6,
+  },
+  {
+    id:7 ,
+    productName:"bike" ,
+    price:10000 ,
+    productImage: product7,
+  },
+  {
+    id:8 ,
+    productName: "bike",
+    price:10000 ,
+    productImage: product8,
+  },
+  {
+    id:9 ,
+    productName:"bike",
+    price:10000 ,
+    productImage: product9,
+  },
+  {
+    id:10 ,
+    productName:"bike",
+    price:10000 ,
+    productImage:product10 ,
+  },
+  {
+    id:11 ,
+    productName: "bike",
+    price:10000 ,
+    productImage: product11,
+  },
+]
